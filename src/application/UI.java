2b5824ab3c603fdb7414570ef5c1e0dc8f271c22
Daniel Coelho <coelhodaniel06087@gmail.com>
2024-08-28 16:13:21 -0300

    Knight

--- src/application/UI.java
+++ src/application/UI.java
@@ -35,11 +35,11 @@ public class UI {
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 	
 	// https://stackoverflow.com/questions/2979383/java-clear-the-console
-		public static void clearScreen() {
-			System.out.print("\033[H\033[2J");
-			System.out.flush();
-		}	
-
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}	
+	
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
 			String s = sc.nextLine();
@@ -52,24 +52,24 @@ public class UI {
 		}
 	}
 	
-	public static void printMatch(ChessMatch chessmatch, List<ChessPiece> captured){
-		printBoard(chessmatch.getPieces());
+	public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
+		printBoard(chessMatch.getPieces());
 		System.out.println();
-		printCapturedPiece(captured);
+		printCapturedPieces(captured);
 		System.out.println();
-		System.out.println("Turn: " + chessmatch.getTurn() );
-		if(!chessmatch.getCheckMate()) {
-		System.out.println("Waiting player: " + chessmatch.getCurrentPlayer());
-			if(chessmatch.getCheck()) {
-				System.out.println("!CHECK!");
+		System.out.println("Turn : " + chessMatch.getTurn());
+		if (!chessMatch.getCheckMate()) {
+			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+			if (chessMatch.getCheck()) {
+				System.out.println("CHECK!");
 			}
 		}
 		else {
-			System.out.println("!!CHECKMATE!!");
-			System.out.println("!!!WINNER!!! -> " + chessmatch.getCurrentPlayer());
+			System.out.println("CHECKMATE!");
+			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
 		}
-		
-	}	
+	}
+	
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
@@ -80,7 +80,7 @@ public class UI {
 		}
 		System.out.println("  a b c d e f g h");
 	}
-	
+
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
@@ -93,10 +93,10 @@ public class UI {
 	}
 
 	private static void printPiece(ChessPiece piece, boolean background) {
-    	if(background) {
-    		System.out.print(ANSI_BLUE_BACKGROUND );
-    	}
-		if (piece == null) {
+		if (background) {
+			System.out.print(ANSI_BLUE_BACKGROUND);
+		}
+    	if (piece == null) {
             System.out.print("-" + ANSI_RESET);
         }
         else {
@@ -108,20 +108,19 @@ public class UI {
             }
         }
         System.out.print(" ");
-        
-     
-        }
-	private static void printCapturedPiece( List<ChessPiece> captured) {
-    	List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
-    	List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
-    	System.out.println("Captured pieces: ");
-    	System.out.print("White: ");
-    	System.out.print(ANSI_WHITE);
-    	System.out.println(Arrays.toString(white.toArray()));
-    	System.out.print(ANSI_RESET);
-    	System.out.print("Black: ");
-    	System.out.print(ANSI_YELLOW);
-    	System.out.println(Arrays.toString(black.toArray()));
-    	System.out.print(ANSI_RESET);
+	}
+	
+	private static void printCapturedPieces(List<ChessPiece> captured) {
+		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
+		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
+		System.out.println("Captured pieces:");
+		System.out.print("White: ");
+		System.out.print(ANSI_WHITE);
+		System.out.println(Arrays.toString(white.toArray()));
+		System.out.print(ANSI_RESET);
+		System.out.print("Black: ");
+		System.out.print(ANSI_YELLOW);
+		System.out.println(Arrays.toString(black.toArray()));
+		System.out.print(ANSI_RESET);
 	}
 }
